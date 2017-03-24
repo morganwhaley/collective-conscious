8dd3f274e739eaac895abc9b22fad8e6e4d0275a
Morgan Whaley <morganwhaley@Morgans-Personal-Macbook.local>
2017-03-24 13:02:57 -0600

    Basic input and styles added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import '../assets/stylesheets/base.scss';
 import React, { Component } from 'react';
+import SearchForm from './search_form';
 
 class App extends Component {
   render() {
     return(
-      <h1>Hello, {this.props.name}!</h1>
+      <div>
+        <h1>{this.props.name}</h1>
+        <h3>Mind Control with Github</h3>
+        <SearchForm />
+      </div>
     )
   }
 };
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 
-render(<App name='World'/>, document.getElementById('root'));
+render(
+  <App name='Collective Conscious' />, document.getElementById('root')
+);
